ebcec57e2138b16432fed97b3a6ac59f4693240f
SergeyZhiganov <isip_s.a.zhiganov@mpt.ru>
2024-05-13 16:09:51 +0300

    Практа

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,96 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+record Product(String name) {
+}
+
+record EmployeePosition(String position) {
+}
+
+record Equipment(String name, int quantity) {
+}
+
+record Material(String name) {
+}
+
+record Service(String name) {
+}
 
 public class Main {
     public static void main(String[] args) {
-        List<String> ProductList = ListProduct();
-        print(ProductList);
-        List<String> DolznotbList = SpisokDolznostey();
-        print(DolznotbList);
-        List<String> KolichestvoStankov = StankiQ();
-        print(KolichestvoStankov);
-        List<String> Material = Materials();
-        print(Material);
-        List <String> Services = Uslugi();
-        print(Services);
-    }
-    static void print(List<String> list) {
-        for (Object element : list.toArray())
-            System.out.println(element);
-    }
-    public static List<String> ListProduct()
-    {
-        return new ArrayList<>(Arrays.asList("Продукция:", "Диван", "Кровать", "Гумибер", "Максим", "Табуретка"));
-    }
-    public static List<String> SpisokDolznostey()
-    {
-        return new ArrayList<>(Arrays.asList("Список должностей:", "Аудитор", "Аналитик", "Администратор", "Бухгалтер", "Диспетчер"));
-    }
-    public static List<String> StankiQ()
-    {
-        return new ArrayList<>(Arrays.asList("Количество рабочих станков:", "15", "105", "666", "23", "32"));
-    }
-
-    public static List <String> Materials()
-    {
-        return new ArrayList<>(Arrays.asList("Материалы: ", "Цельный массив дерева", "Клееный массив дерева", "Фанера", "Древесно-стружечная плита"));
+        List<Product> productList = getProductList();
+        System.out.println("Мебель:");
+        for (Product product : productList) {
+            System.out.println(product.name());
+        }
+        List<EmployeePosition> employeePositions = getEmployeePositions();
+        System.out.println("Должности:");
+        for (EmployeePosition employeePosition : employeePositions) {
+            System.out.println(employeePosition.position());
+        }
+        System.out.println("Станки:");
+        List<Equipment> equipmentList = getEquipmentList();
+        for (Equipment equipment : equipmentList) {
+            System.out.println(equipment.name() + ": " + equipment.quantity());
+        }
+        System.out.println("Материалы:");
+        List<Material> materials = getMaterials();
+        for (Material material : materials) {
+            System.out.println(material.name());
+        }
+        System.out.println("Услуги:");
+        List<Service> services = getServices();
+        for (Service service : services) {
+            System.out.println(service.name());
+        }
     }
 
-    public static List <String> Uslugi()
-    {
-        return new ArrayList<>(Arrays.asList("Распил", "Кромление", "Покраска", "Фрезеровка", "Присадка"));
+    public static List<Product> getProductList() {
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product("Диван"));
+        productList.add(new Product("Кровать"));
+        productList.add(new Product("Гумибер"));
+        productList.add(new Product("Максим"));
+        productList.add(new Product("Табуретка"));
+        return productList;
+    }
+
+    public static List<EmployeePosition> getEmployeePositions() {
+        List<EmployeePosition> employeePositions = new ArrayList<>();
+        employeePositions.add(new EmployeePosition("Аудитор"));
+        employeePositions.add(new EmployeePosition("Аналитик"));
+        employeePositions.add(new EmployeePosition("Администратор"));
+        employeePositions.add(new EmployeePosition("Бухгалтер"));
+        employeePositions.add(new EmployeePosition("Диспетчер"));
+        return employeePositions;
+    }
+
+    public static List<Equipment> getEquipmentList() {
+        List<Equipment> equipmentList = new ArrayList<>();
+        equipmentList.add(new Equipment("Количество токарных станков", 15));
+        equipmentList.add(new Equipment("Количество фрезерных станков", 105));
+        equipmentList.add(new Equipment("Количество сверлильных станков", 666));
+        equipmentList.add(new Equipment("Количество лазерных станков", 23));
+        equipmentList.add(new Equipment("Количество пресс-станков", 32));
+        return equipmentList;
+    }
+
+    public static List<Material> getMaterials() {
+        List<Material> materials = new ArrayList<>();
+        materials.add(new Material("Цельный массив дерева"));
+        materials.add(new Material("Клееный массив дерева"));
+        materials.add(new Material("Фанера"));
+        materials.add(new Material("Древесно-стружечная плита"));
+        return materials;
+    }
+
+    public static List<Service> getServices() {
+        List<Service> services = new ArrayList<>();
+        services.add(new Service("Распил"));
+        services.add(new Service("Кромление"));
+        services.add(new Service("Покраска"));
+        services.add(new Service("Фрезеровка"));
+        services.add(new Service("Присадка"));
+        return services;
     }
 }
